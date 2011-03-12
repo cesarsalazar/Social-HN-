35076076e6930227c17d413a086a77ff6def24d4
César Salazar <i.am@cesarsalzar.mx>
2011-03-11 22:56:51 -0600

    Now rows are inserted correctly. Still single user support

--- hn.js
+++ hn.js
@@ -3,10 +3,34 @@ $(function(){
   function manipulateNewsFeed(data) {
     var rows = '';
     $.each(data.items, function(index, item){
-      var row = '<p><a href="'+item.url+'">'+item.title+'</a></p><p>'+item.postedBy+'</p>';
+      item.domain = jQuery.url.setUrl(item.url).attr("host");  
+      var row =   '<tr>'
+              +     '<td align=right valign=top class="title">X.</td>'
+              +     '<td>'
+              +       '<center>'
+              +         '<a id=up_2314532 href="item?id='+item.id+'" target="blank">'
+              +           '<img src="http://ycombinator.com/images/grayarrow.gif" border=0 vspace=3 hspace=2>'
+              +         '</a>'
+              +         '<span id=down_2314532></span>'
+              +       '</center>'
+              +     '</td>'
+              +     '<td class="title">'
+              +       '<a href="'+item.url+'">'+item.title+'</a>'
+              +       '<span class="comhead"> ('+item.domain+') </span>'
+              +     '</td>'
+              +   '</tr>'
+              +   '<tr>'
+              +     '<td colspan=2></td>'
+              +     '<td class="subtext">'
+              +       '<span id=score_2314532>'+item.points+' points</span> by <a href="user?id='+item.postedBy+'">'+item.postedBy+'</a> '+item.postedAgo+'  | <a href="item?id='+item.id+'">'+item.commentCount+' comments</a>'
+              +     '</td>'
+              +   '</tr>'
+              +   '<tr style="height:5px"></tr>';
+      
       rows += row;
+          
     });
-    $('table').before(rows); 
+    $('tbody').eq(2).prepend(rows); 
   };
   
   chrome.extension.sendRequest({'action' : 'fetchNews'}, manipulateNewsFeed);
